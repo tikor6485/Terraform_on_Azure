fd9bc98b3c2a8507b2ea8b14e01d0d248be7b952
Tirdad Koraei <tikor6485@yahoo.com>
2026-01-01 10:57:51 +0100

    foundation: clean bootstrap state artifacts and simplify backend-template

--- 00-foundation/01-remote-state-azure-storage/backend-template/providers.tf
+++
@@ -1,6 +0,0 @@
-provider "azurerm" {
-  features {}
-
-  # If set, Terraform uses this subscription explicitly; otherwise Azure CLI context is used.
-  subscription_id = var.subscription_id != "" ? var.subscription_id : null
-}
